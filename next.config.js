d4eea31e2a776b7ef84203478d7a4006bb1426c0
Bruno Siqueira <bbrunosiqueira14@gmail.com>
2026-04-01 17:29:14 -0300

    feat:link-association

--- next.config.js
+++ next.config.js
@@ -2,6 +2,28 @@
 const nextConfig = {
   reactStrictMode: true,
   pageExtensions: ['page.tsx', 'page.ts', 'routes.ts'],
+  async headers() {
+    return [
+      {
+        source: '/apple-app-site-association',
+        headers: [
+          {
+            key: 'Content-Type',
+            value: 'application/json; charset=utf-8',
+          },
+        ],
+      },
+      {
+        source: '/.well-known/apple-app-site-association',
+        headers: [
+          {
+            key: 'Content-Type',
+            value: 'application/json; charset=utf-8',
+          },
+        ],
+      },
+    ]
+  },
   images: {
     remotePatterns: [
       {
